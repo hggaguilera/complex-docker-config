fb44fb6d2ec5ab87a45811af25cb03ce88e66717
Gabriel Gonzalez <hgonzalez.leis@gmail.com>
2021-05-21 10:23:45 -0600

    testing build

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './app';
 
+// I just add a comment
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Fib Calculator/i);
